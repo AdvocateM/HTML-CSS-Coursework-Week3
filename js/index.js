d155973e8544116536fdfeb95b98b3ae3434620e
mr_maroga <45645052+AdvocateM@users.noreply.github.com>
2022-06-28 07:45:05 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  var x = document.getElementById("nav");
+  if (x.style.display === "block") {
+    x.style.display += " responsive";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function x() {
+  var x = document.getElementById("foot");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
